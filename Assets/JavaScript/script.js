f6c092e3fd8818afba2005387d7981d8c8ea41cc
Chris Abiva <chrisabiva@hotmail.com>
2021-05-08 19:32:26 -0700

    Playing with Flexbox

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -22,6 +22,7 @@ $('#btn-search').on('click', function() {
   }
 
   searchApi(searchVal);
+  searchForcastApi(searchVal);
 })
 
 // 
@@ -44,44 +45,69 @@ function searchApi(txtSearchEl) {
     .then(res => res.json()) 
 
     .then(data => { 
-      // console.log(data)
+      // Clearing data before searching again
       $('#card-weather').empty();
+      // $('#weather-forecast').empty();
+
       let card = $('<div>').addClass('card-body');
-      let cardCity = $('<h1>').addClass('card-title').text(`${data.city.name}`);
+      let cardCity = $('<h2>').addClass('card-city').text(`${data.city.name}`);
       let title = $('<h5>').addClass('card-today').text(today);
       let temp = $('<p>').addClass('card-temp').text(`Temp: ${data.list[0].main.temp}°F`);
       let wind = $('<p>').addClass('card-wind').text(`Wind: ${data.list[0].wind.speed}M/H`);
       let humid = $('<p>').addClass('card-humid').text(`Humid: ${data.list[0].main.humidity}%`);
-      let icon = $('<img>').addClass('card-icon').attr({ src: `./Assets/Images/${data.list[0].weather[0].icon}.png` });
+      let icon = $('<img>').addClass('card-icon').attr({ src: `./Assets/Images/${data.list[0].weather[0].icon}.png`});
       card.append(cardCity, title, temp, wind, humid);
-      // Variable Below
-      let cardForecastDiv = $('<div>').addClass('d-flex justify-content-around weather-card-container');
       $('#card-weather').append(card);
-      $('.card-title').append(icon)
-
-
-      for ( let x = 1; x <= 5 ; x++)
-      console.log(x)
-
+      $('.card-city').append(icon);
       
-      let cardForecast = $('<div>').addClass('dcard text-center weather-card');
-      let dateForecast = $('<h5>').addClass('card-today-forecast').text(`${data.list[x].dt_txt}`);
-      let tempForecast = $('<p>').addClass('card-temp').text(`Temp: ${data.list[x].main.temp}°F`);
-      let windForecast = $('<p>').addClass('card-wind').text(`Wind: ${data.list[x].wind.speed}M/H`);
-      let humidForecast = $('<p>').addClass('card-humid').text(`Humid: ${data.list[x].main.humidity}%`);
-      // let iconForecast = $('<img>').addClass('card-icon').attr({ src: `./Assets/Images/${data.list[x].weather[0].icon}.png` });
-      cardForecastDiv.append(cardForecast);
-      cardForecast.append(cardCity, dateForecast, tempForecast, windForecast, humidForecast);
-      // $('.card-today-forecast').append(iconForecast)
 
-    })
+  })
+
     .catch(function (error) {
       console.error(error);
     });
+
+    secondFetch(txtSearchEl);
 }
 
+function secondFetch(txtSearchEl) {
+  const request = `http://api.openweathermap.org/data/2.5/forecast?q=${txtSearchEl}&units=imperial&appid=904d57a990eb1f14578feeb79ec45ef2`;
+  $('#five-day-forecast').css('visibility', 'visible');
 
-// api.openweathermap.org/data/2.5/weather?q=seattle&appid=904d57a990eb1f14578feeb79ec45ef2
+  fetch(request)
+    .then(res => res.json()) 
+
+    .then(data => { 
+      // Clearing data before searching again
+      $('#five-day-forecast').empty();
+
+      let cardForecastDiv = $('<div>').addClass('d-flex justify-content-end align-self-start');
+
+
+      for ( let x = 0; x <= 40 ; x+=8) {
+      let date = data.list[x].dt_txt.split(' ')[0];
+      console.log(date);
+
+      let cardForecast = $('<div>').addClass('cards card');
+      let dateForecast = $('<h5>').addClass('card-today-forecast').text(`${date}`);
+      let tempForecast = $('<p>').addClass('card-temp').text(`Temp: ${data.list[x].main.temp}°F`);
+      let windForecast = $('<p>').addClass('card-wind').text(`Wind: ${data.list[x].wind.speed}M/H`);
+      let humidForecast = $('<p>').addClass('card-humid').text(`Humid: ${data.list[x].main.humidity}%`);
+      let iconForecast = $('<img>').addClass('card-icon').attr({ src: `./Assets/Images/${data.list[x].weather[0].icon}.png` });
+
+      cardForecast.append(dateForecast,iconForecast, tempForecast, windForecast, humidForecast);
+      cardForecastDiv.append(cardForecast);
+
+      $('#five-day-forecast').append(cardForecastDiv);
+  }
+})
+    .catch(function (error) {
+      console.error(error);
+    });
+
+
+
+  }
 
 // Check if search has a valid input
 function validInput() {
@@ -95,40 +121,3 @@ function validInput() {
 
   searchApi(txtSearchEl);
 }
-
-
-// btnSearchEl.onclick = validInput;
-
-
-
-
-      // const tempApi = data.main.temp;
-      // const humidApi = data.main.humidity;
-      // const windApi = data.wind.speed;
-      // const cityApi = data.name;
-      
-      // Kelvin to Farenheit
-      // const tempFar = (tempApi - 273.15) * 9/5 + 32;
-      
-      // Display to the DOM
-      // tempResultEl.textContent = tempFar.toFixed(2);
-      // humidityResultEl.textContent = humidApi;
-      // windResultEl.textContent = windApi.toFixed(1);
-      // cityResultEl.textContent = cityApi;
-      // write query to page so user knows what they are viewing
-      
-      
-
-      // resultTextEl.textContent = locRes.search.query;
-
-      // console.log(locRes);
-
-      // if (!locRes.results.length) {
-      //   console.log('No results found!');
-      //   resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-      // } else {
-      //   resultContentEl.textContent = '';
-      //   for (var i = 0; i < locRes.results.length; i++) {
-      //     printResults(locRes.results[i]);
-      //   }
-      // }
